debdf1ecadc0a16a5c217c728fd5bf796166e92e
tranvinhthinh <tranvinhthinh@luvina.net>
2018-12-25 14:52:37 +0700

    fix  :   hibernate.cfg.xml file : mapping class not work

--- src/main/java/thinhtv/training/Utils/HibernateUtil.java
+++ src/main/java/thinhtv/training/Utils/HibernateUtil.java
@@ -1,9 +1,12 @@
 package thinhtv.training.Utils;
 
+import java.util.Set;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+import org.reflections.Reflections;
 
 public class HibernateUtil {
 	private static SessionFactory sessionFactory;
@@ -14,7 +17,16 @@ public class HibernateUtil {
 	public static SessionFactory getSessionFactory() {
 		
 		Configuration configuration = new Configuration().configure();
-		configuration.addAnnotatedClass(thinhtv.training.models.Movie.class);
+		//configuration.addAnnotatedClass(thinhtv.training.models.Movie.class);
+		
+		Reflections reflections = new Reflections("thinhtv.training.models");
+
+		Set<Class<?>> classes = reflections.getTypesAnnotatedWith(javax.persistence.Entity.class);
+
+		for(Class<?> clazz : classes)
+		{
+		    configuration.addAnnotatedClass(clazz);
+		}
 		
 		StandardServiceRegistryBuilder srb = new StandardServiceRegistryBuilder();
 		srb.applySettings(configuration.getProperties());
