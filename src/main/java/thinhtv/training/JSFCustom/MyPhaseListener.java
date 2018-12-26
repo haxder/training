c232b04cf0258cccf6edec9da4ec3bfb56c62df8
tranvinhthinh <tranvinhthinh@luvina.net>
2018-12-26 10:44:18 +0700

    add phase listener

---
+++ src/main/java/thinhtv/training/JSFCustom/MyPhaseListener.java
@@ -0,0 +1,27 @@
+package thinhtv.training.JSFCustom;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+public class MyPhaseListener implements PhaseListener {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		// TODO Auto-generated method stub
+		System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆ after Phase ("+ event.getPhaseId() +")☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent event) {
+		System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆ before Phase ("+ event.getPhaseId() +")☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.ANY_PHASE;
+	}
+
+}
--- src/main/java/thinhtv/training/controller/MovieController.java
+++ src/main/java/thinhtv/training/controller/MovieController.java
@@ -3,15 +3,11 @@ package thinhtv.training.controller;
 import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseId;
-
 import org.hibernate.Session;
-
 import thinhtv.training.Utils.HibernateUtil;
 import thinhtv.training.manager.MoviesDataManager;
 import thinhtv.training.models.Movie;
@@ -22,8 +18,6 @@ public class MovieController implements Serializable{
 	private static final long serialVersionUID = -2788199977054986476L;
 	private List<Movie> movies = null;
 
-	@SuppressWarnings("unchecked")
-	@PostConstruct
 	public void init() {
 		if ((FacesContext.getCurrentInstance().getCurrentPhaseId() == PhaseId.RENDER_RESPONSE)) {
 			Session session = HibernateUtil.getSessionFactory().openSession();
