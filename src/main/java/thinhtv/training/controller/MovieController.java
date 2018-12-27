849d6f821d193766c52b29ca8f4f0749f0e2b9e1
tranvinhthinh <tranvinhthinh@luvina.net>
2018-12-27 10:04:35 +0700

    sử dụng lazyload trong primeface
    (băt buộc phải set  setRowCount   thì mới load được data đúng)

--- src/main/java/thinhtv/training/controller/MovieController.java
+++ src/main/java/thinhtv/training/controller/MovieController.java
@@ -1,38 +1,43 @@
 package thinhtv.training.controller;
 
 import java.io.Serializable;
-import java.sql.SQLException;
-import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.PhaseId;
 
-import org.hibernate.Session;
+import org.primefaces.model.LazyDataModel;
 
-import thinhtv.training.Utils.HibernateUtil;
-import thinhtv.training.manager.MoviesDataManager;
 import thinhtv.training.models.Movie;
+import thinhtv.training.models.lazyDataModels.MoviesLazyDataModel;
 
 @ManagedBean(name = "movieController")
 @ViewScoped
 public class MovieController implements Serializable{
 	private static final long serialVersionUID = -2788199977054986476L;
-	private List<Movie> movies = null;
+	//private List<Movie> movies = null;
+	
+	private LazyDataModel<Movie> movies = new MoviesLazyDataModel();
 
 	public void init() {
-		if ((FacesContext.getCurrentInstance().getCurrentPhaseId() == PhaseId.RENDER_RESPONSE)) {
+/*		if ((FacesContext.getCurrentInstance().getCurrentPhaseId() == PhaseId.RENDER_RESPONSE)) {
 			Session session = HibernateUtil.getSessionFactory().openSession();
 			movies = session.createQuery("From MOVIES", Movie.class).list();
 			session.close();
-		}
+		}*/
+	}
+
+	public LazyDataModel<Movie> getMovies() {
+		return movies;
+	}
+
+	public void setMovies(LazyDataModel<Movie> movies) {
+		this.movies = movies;
 	}
 	
 	/** 
 	 * lấy data theo sql thuần 
 	 */
-	public String selectAllMovies() throws SQLException, ClassNotFoundException {
+/*	public String selectAllMovies() throws SQLException, ClassNotFoundException {
 
 		MoviesDataManager manager = new MoviesDataManager();
 		movies = manager.selectAllMovies();
@@ -45,5 +50,6 @@ public class MovieController implements Serializable{
 
 	public void setMovies(List<Movie> movies) {
 		this.movies = movies;
-	}
+	}*/
+	
 }
---
+++ src/main/java/thinhtv/training/models/lazyDataModels/MoviesLazyDataModel.java
@@ -0,0 +1,33 @@
+package thinhtv.training.models.lazyDataModels;
+import java.util.List;
+import java.util.Map;
+import org.hibernate.Session;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+import thinhtv.training.Utils.HibernateUtil;
+import thinhtv.training.models.Movie;
+
+public class MoviesLazyDataModel extends LazyDataModel<Movie> {
+	
+	private static final long serialVersionUID = -4836640077175973670L;
+	
+	private Session session = HibernateUtil.getSessionFactory().openSession();
+	
+	public MoviesLazyDataModel() {
+		Long maxRow = session.createQuery("Select count(*) From MOVIES", Long.class).getSingleResult();
+		
+		// bắt buộc phải khởi tạo được max row trong db! không thì sẽ ko tính được vị trí cần lấy
+		
+		this.setRowCount(maxRow.intValue());
+		System.out.println("☆☆" + maxRow + "☆☆");
+	}
+
+	@Override
+	public List<Movie> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+		List<Movie> movies = session.createQuery("From MOVIES", Movie.class)
+				.setFirstResult(first)
+				.setMaxResults(pageSize)
+				.getResultList();
+		return movies;
+	}
+}
